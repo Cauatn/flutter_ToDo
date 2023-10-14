e3223193716bb9668b66e63a59dad9954e6b3a79
Cauã Tavares <tavarescauac@gmail.com>
2023-10-14 11:39:27 -0300

    refactoring and adding TodoTile constructor values

--- lib/components/todo_tile.dart
+++ lib/components/todo_tile.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  const TodoTile({Key? key}) : super(key: key);
+  final String tarefaNome;
+  final bool tarefaValue;
+  Function(bool?)? onChanged;
+
+  TodoTile({
+    super.key,
+    required this.tarefaNome,
+    required this.tarefaValue,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: EdgeInsets.all(24),
-        child: Text('aprendendo flutter'),
         decoration: BoxDecoration(
             color: Colors.purple, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(
+              value: tarefaValue,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
+            Text(tarefaNome),
+          ],
+        ),
       ),
     );
   }
